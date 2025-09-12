56a03f0aa38f64345012c49deeaea82e4c4c8b65
Wes Marc <weslei047@gmail.com>
2025-09-12 03:11:35 -0300

    feat: Add logout feature and session destroy

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { home, register, login } from "./controllers/userController.js";
+import { home, register, login, logout } from "./controllers/userController.js";
 
 const router = Router();
 
 router.get("/", home);
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
